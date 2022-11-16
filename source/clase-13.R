e67c255907ff11a6aed54ebbc93c5180fec5ae96
DavidChaparro11 <d.chaparro@uniandes.edu.co>
2022-11-16 18:14:54 -0500

    a

--- source/clase-13.R
+++ source/clase-13.R
@@ -9,8 +9,9 @@ p_load(tidyverse, # contiene las librerías ggplot, dplyr...
 
 ## Acceder al robots.txt de wikipedia
 browseURL("https://en.wikipedia.org/robots.txt")
-
+browseURL("https://github.com/eduard-martinez/robots.txt")
 ## Mi primer HTML
+## párrafo de html <p>. para cerralo uno le poner un slash antes el nombre del elemento
 my_html <- 
 '<!DOCTYPE html> 
 <html>
@@ -38,35 +39,67 @@ my_url = "https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol"
 browseURL(my_url) ## Ir a la página
 
 ## leer el html de la página
-
+my_html <- read_html(my_url)
 
 ## ver la clase del objeto
 
-
+class(my_html)
 ## ver el objeto
 
 
 ## 2.2 Extraer elementos de un HTML
-
+##copiammos el xpath mediante inspeccionar
 
 ## Obtener los elementos h2 de la página
+##html_
+h2_fifa <-  my_html %>% html_nodes("h2")
 
 
 ## Ver los textos
-
+h2_fifa %>% html_text()
+my_html %>% html_elements("h2") %>% html_text()
 
 ## 2.3 Xpath
 
+html_p <- my_html %>% html_nodes(xpath= '//*[@id="mw-content-text"]/div[1]/p[5]')##debe ser con una sola comilla porque dentro del texto hay comillas. Es necesario aclarar que es un xpath
+my_html %>% html_nodes(xpath= '//*[@id="mw-content-text"]/div[1]/p[5]') %>% html_text() ##ponerlo en texto
+
+
+## extraer los links dentro del párrafo
+html_a <- html_p %>% html_elements("a")
+html_a
+html_a %>% html_attr("href") ##el atributo href es el que contiene el link. eso se puede vwer en el html
+urls_p <- paste0("https://es.wikipedia.org/", html_a %>% html_attr("href"))
+
+urls_p
 
 ## extraer todas las tablas del html 
-
+my_table = my_html %>% html_table()
 
 ## numero de tablas extraidas
+length(my_table)
 
+tabla_jug <- my_table[[11]]
 
 ## tabla 10
 
 
+sub_html = my_html %>% html_nodes(xpath='//*[@id="mw-content-text"]/div[1]/table[10]/tbody')
+class(sub_html)
 
+elements = sub_html %>% html_nodes("a")
+elements[1:5]
+
+titles = elements %>% html_attr("title")
+titles[1:5]
+
+refs = elements %>% html_attr("href")
+refs[1:5]
+
+db = tibble(titles,url=paste0("https://es.wikipedia.org",refs))
+db %>% head()
+
+browseURL(db$url[10])
+lu
 
 
